9262523826d90fccbf606a5da6822539d9993165
Fehmi Bora Şimşek <vicdumsimsek@gmail.com>
2024-09-16 11:15:36 +0300

    Add getDepartmentData function to fetch and save department data

--- app.js
+++ app.js
@@ -28,7 +28,31 @@ async function getCourseData() {
     }
 }
 
+async function getDepartmentData() {
+    const url = 'https://schedule.atilim.edu.tr/public/departments';
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(JSON.stringify({ error: 'Network response was not ok', statusText: response.statusText, statusCode: response.status }));
+        }
+        const data = await response.json();
+        const filePath = path.join(__dirname, 'tmp', 'departmentData.json');
+        let localData = null;
 
+        if (fs.existsSync(filePath)) {
+            const localFileContent = fs.readFileSync(filePath, 'utf-8');
+            localData = JSON.parse(localFileContent);
+        }
+        if (JSON.stringify(localData) === JSON.stringify(data)) {
+            console.log(JSON.stringify({ message: 'Data has not changed. No need to save.', statusCode: 304 }));
+            return;
+        }
+        fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf-8');
+        console.log(JSON.stringify({ message: 'Data has been saved to departmentData.json', statusCode: 200 }));
+    } catch (error) {
+        console.error(JSON.stringify({ error: 'There has been a problem with your fetch operation', details: error.message, statusCode: error.statusCode || 500 }));
+    }
+}
 
 const app = express();
 const port = 3000;
@@ -42,3 +66,4 @@ app.listen(port, () => {
 });
 
 getCourseData();
+getDepartmentData();
